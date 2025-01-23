470ff3007e9d692dc3eb46d592d5fcbc0a31ace4
Fabricio Borgobello <fborgobello@gmail.com>
2025-01-23 18:46:59 +0100

    Exchange seed

--- src/db/scripts/exchanges/seed.ts
+++ src/db/scripts/exchanges/seed.ts
@@ -1,7 +1,25 @@
-import { db } from "../..";
+import { db } from "@/db";
+
+import rates2021 from "./2021.json";
+import rates2022 from "./2022.json";
+import rates2023 from "./2023.json";
+import rates2024 from "./2024.json";
+import rates2025 from "./2025.json";
 import { ExchangeRateInsert, exchangeRates } from "../../schema/exchange_rates";
 
-const rates: ExchangeRateInsert[] = [];
+const typed2021 = rates2021 as { rates: ExchangeRateInsert[] };
+const typed2022 = rates2022 as { rates: ExchangeRateInsert[] };
+const typed2023 = rates2023 as { rates: ExchangeRateInsert[] };
+const typed2024 = rates2024 as { rates: ExchangeRateInsert[] };
+const typed2025 = rates2025 as { rates: ExchangeRateInsert[] };
+
+const rates: ExchangeRateInsert[] = [
+  ...typed2021.rates,
+  ...typed2022.rates,
+  ...typed2023.rates,
+  ...typed2024.rates,
+  ...typed2025.rates,
+];
 
 // Read JSON file and process it
 const processJson = async () => {
